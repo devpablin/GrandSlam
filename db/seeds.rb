f791fef42bb81982939f65d839c2ec6cb66c0b99
DevPablin <pablingonzales@gmail.com>
2018-11-27 18:12:39 -0400

    adsa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  Pmplayer.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    fechanacimiento: Faker::Date.birthday(18, 40)
+  )
+end
+
+50.times do
+  Pmtrainer.create(
+    nombre: Faker::Name.first_name,
+    apellido: Faker::Name.last_name,
+  )
+end
+
+30.times do
+  Pmnationality.create(
+    name: Faker::Nation.nationality
+  )
+end
+
+40.times do
+  Pmcountry.create(
+    name: Faker::Address.country
+  )
+end
+
+50.times do
+  Pmreferee.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+  )
+end
